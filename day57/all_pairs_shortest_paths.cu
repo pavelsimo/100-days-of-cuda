b12ef30e16c96158c5d724252ff4e2e04830874a
Pavel Simo <pavel.simo@gmail.com>
2026-09-13 20:15:03 +0200

    📝 update README.md: day 57

--- day57/all_pairs_shortest_paths.cu
+++ day57/all_pairs_shortest_paths.cu
@@ -15,12 +15,11 @@ __global__ void floyd_warshall(float* dist, int N, int k) {
 // dist, output are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const float* dist, float* output, int N) {
     cudaMemcpy(output, dist, N * N * sizeof(float), cudaMemcpyDeviceToDevice);
-    dim3 threadsPerBlock(16, 16);
-    dim3 numBlocks((N + threadsPerBlock.x - 1) / threadsPerBlock.x,
-                   (N + threadsPerBlock.y - 1) / threadsPerBlock.y);
+    dim3 threads(16, 16);
+    dim3 blocks((N + threads.x - 1) / threads.x,
+                (N + threads.y - 1) / threads.y);
     for (int k = 0; k < N; k++) {
-        floyd_warshall<<<numBlocks, threadsPerBlock>>>(output, N, k);
+        floyd_warshall<<<blocks, threads>>>(output, N, k);
     }
-
     cudaDeviceSynchronize();
 }
